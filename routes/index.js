992fc9234c71f8e5f18dd1f94b209f9cc9cff77c
Leevi <leewi.98@hotmail.com>
2023-01-20 14:44:02 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,7 @@ router.post('/api/user/login', checkNotAuthenticated, passport.authenticate('loc
 router.post('/api/todos', checkAuthenticated,  (req,res) => {
   todobody = req.body.todo;
   if(todobody == null) {
+    console.log(req.body)
     res.send("Body was empty")
   } else {
   todojson = {
