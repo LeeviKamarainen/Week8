69bbe95250e7113c748150d514fa476fd7e04166
Leevi <leewi.98@hotmail.com>
2023-01-22 10:34:35 +0200

    test2

--- routes/index.js
+++ routes/index.js
@@ -39,9 +39,11 @@ router.post('/api/user/login', checkNotAuthenticated, passport.authenticate('loc
 
 router.post('/api/todos', checkAuthenticated,  (req,res) => {
   todobody = req.body.todo;
+  console.log(todobody)
+  console.log(req.user)
   todojson = {
     "id": req.user.id,
-    "todo": [req.body.todo]
+    "todo": [todobody]
   }
   let foundflag = 0;
   let foundindex = 0;
